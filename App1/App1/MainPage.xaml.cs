b64c5bb1a84af9fb0d76aaf722521b0052b1d82f
Thalles <thallescaltabiano@gmail.com>
2020-04-08 13:34:32 -0300

    Aplicação de Consulta de Endereço por CEP

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1.Servico.Modelo;
+using App1.Servico;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+
+            // To Do - validações 
+            string cep = CEP.Text.Trim(); // Trim remove os espaços iniciais e finais da string CEP digitada pelo usuário
+            if (ValidarCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep); // endereço do cep forneciodo pelo usuário
+                    if(string.IsNullOrEmpty(end.cep))
+                    {
+                        DisplayAlert("Erro", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+                    
+                    if (String.IsNullOrEmpty(end.bairro))
+                    {
+                        RESULTADO.Text = string.Format("Endereço: Rua: {0} , UF:{1}", end.localidade, end.uf); // exibição do resultado
+                    }
+                    else
+                    {
+                        RESULTADO.Text = string.Format("Endereço: Rua: {0} , Bairro: {1} , UF:{2}", end.localidade, end.bairro, end.uf); // exibição do resultado
+                    }
+                }
+                catch(Exception e)
+                {
+                    DisplayAlert("Erro Crítico", e.Message, "OK");
+                }
+               
+                
+            }
+        }
+
+        private bool ValidarCEP(string cep)
+        {
+            bool valido = true;
+            if(cep.Length != 8)
+            {
+                DisplayAlert("Erro", "Cep inválido! O CEP deve conter 8 caracteres", "OK"); 
+                valido = false;
+                //Erro
+            }
+            int NovoCEP = 0;
+            if(!int.TryParse(cep,out NovoCEP))
+            {
+                //ERRO
+                DisplayAlert("Erro", "Cep inválido! O CEP deve ser composto apenas por números", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ App1/App1/Servico/ViaCepServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App1.Servico
+{
+    class ViaCepServico
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoUrl, cep); // inclui o Cep no campo{0} 
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoUrl); // Conteudo recebe o objeto json retornado pela pagina
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo); // O método converte o conteudo json para as variaveis da classe Endereço
+           
+
+            return end;
+        }
+    }
+}
